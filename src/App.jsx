21fec59dd1500428c710f4e360b88b480237da82
Akbarali Azimov <akbaraliazimov.09@gmail.com>
2026-07-01 16:36:46 -0300

    new upload App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  
-
-  return (
-    <>
-      
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
