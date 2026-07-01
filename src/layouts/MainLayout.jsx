6804c614608c8a4576aa8f61be3e746b661c5d7e
hyprhub <sherzodbekdavronov111@gmail.com>
2026-07-01 23:03:42 +0500

    Complete navbar and footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../common/Navbar";
-import Footer from "../common/Footer";
+import Navbar from "../components/common/Navbar"; // <-- Mana shu yo'l to'g'ri bo'lishi shart!
+import Footer from "../components/common/Footer"; // <-- Mana shu ham
 
 function MainLayout() {
   return (
-    <>
+    <div>
       <Navbar />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
 
