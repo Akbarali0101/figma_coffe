c06af6a33f4a6378880fd55340a8366d0bb7357a
Akbarali Azimov <akbaraliazimov.09@gmail.com>
2026-07-01 19:27:59 -0300

    modal upload

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,27 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../common/Navbar";
-import Footer from "../common/Footer";
+import { useState } from "react";
+
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
+import Modal from "../components/ui/modal/Modal";
 
 function MainLayout() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <Navbar />
+
+      {/* Faqat test uchun */}
+      <button onClick={() => setOpen(true)}>
+        Open Modal
+      </button>
+
       <Outlet />
+
       <Footer />
+
+      {open && <Modal onClose={() => setOpen(false)} />}
     </>
   );
 }
