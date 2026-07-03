77ec2f494c85b61109eb24c49c12e82b3a8a7982
otabek-webcoder <tekinakkaunt45@gmail.com>
2026-07-03 10:08:48 +0500

    otabek-home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/common/Navbar";
+import Header from "../components/common/Header";
 import Chose from "../components/ui/chose/Chose";
 import Our_collection from "../components/ui/our_collection/Our_collect";
-
 import Works from "../components/common/works/Works";
+import Footer from "../components/common/Footer";
 
 
 
