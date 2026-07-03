e2432cdb70dcf93ae33a3a4ecd9be8c2a8935cc7
shabnam-dev7 <sitways5@gmail.com>
2026-07-03 11:51:45 +0500

    added head

--- src/pages/About_us.jsx
+++ src/pages/About_us.jsx
@@ -1,5 +1,3 @@
-import Footer from "../components/common/Footer";
-import Navbar from "../components/common/Navbar";
 import { Commitment } from "../components/ui/commitment/commitment";
 import { Head } from "../components/ui/head/head";
 import { Quality } from "../components/ui/quality/Quality";
@@ -8,11 +6,9 @@ import { Quality } from "../components/ui/quality/Quality";
 function About_us() {
   return (
     <>
-      <Navbar />
       <Commitment />
       <Quality />
       <Head />
-      <Footer />
     </>
   );
 }
