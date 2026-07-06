51c5ba287fc455c9793b749517be787eeb7c21b2
hyprhub <sherzodbekdavronov111@gmail.com>
2026-07-06 23:04:40 +0500

    faylidagi muommolarni hal qildim

--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import Hero from '../components/ui/Hero/Hero';
+import Header from '../components/common/Header';
+// import Hero from '../components/ui/Hero/Hero';
 import Works from '../components/common/works/Works';
 import Coffeesection from '../components/ui/Coffeesection/Coffeesection';
-import PlanSelector from '../components/ui/PlanSelector/PlanSelector'; 
+import PlanSelector from '../components/ui/PlanSelector/PlanSelector';
 import OrderForm from '../components/ui/OrderForm/OrderForm';
 import Confik from '../components/ui/Confik/Confik';
 import Deliver from '../components/ui/Deliver/Deliver';
 import Beat from '../components/ui/Beat/Beat';
+import Chose from '../components/ui/chose/Chose';
 
 const Subscribe = () => {
   return (
     <div>
-      < Hero/>
-      < Works isDark={true}/>
-      < Chose/>
-      < Coffeesection/>
-      < PlanSelector/>
-      < OrderForm/>
-      < Confik/>
-      < Deliver/>
-      < Beat/>
+      <Header backgroundImage="/hero-bg/Object.png"/>
+      {/* <Hero/> */}
+      <Works isDark={true}/>
+      <Chose/>
+      <Coffeesection/>
+      <PlanSelector/>
+      <OrderForm/>
+      <Confik/>
+      <Deliver/>
+      <Beat/>
     </div>
   );
 };
