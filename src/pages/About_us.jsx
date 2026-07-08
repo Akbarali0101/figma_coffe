7cecf07b804901f3eaea47d1305a7fcb2e8edb7c
shabnam-dev7 <sitways5@gmail.com>
2026-07-08 11:00:04 +0500

    merge fix

--- src/pages/About_us.jsx
+++ src/pages/About_us.jsx
@@ -1,6 +1,6 @@
-import { Commitment } from "../components/ui/commitment/commitment";
-import { Head } from "../components/ui/head/head";
-import { Quality } from "../components/ui/quality/Quality";
+import { Commitment } from "../components/ui/commitment — копия/commitment";
+import { Head } from "../components/ui/head — копия/Head";
+import { Quality } from "../components/ui/quality — копия/Quality";
 
 
 function About_us() {
